b09c861b3d79211162336c934510480ea6e19e01
Itamar Shabtai <itamar.shabtai.1@gmail.com>
2022-09-27 00:32:48 +0300

    Add DragonBlade and Dragon's Bulwark

---
+++ gear/trinkets.py
@@ -0,0 +1,3 @@
+import misc
+
+dragons_bulwark = misc.Item("Dragon's Bulwark", {"bpd": 5, "crit": 5, "mpm": 5, "WIS": 15, "END": 15, "INT": 6, "DEX": 6, "STR": 6, "bonus": 5}, {"light": 5, "darkness": 5, "all": 3, "health": -3}, )
--- gear/weapons.py
+++ gear/weapons.py
@@ -4,3 +4,4 @@ import constants
 weaver_blade = misc.Weapon("Weaver Blade", constants.DMG_TYPE_SCYTHE, "metal", 20, 40, {"bonus": 10, "crit": 5}, {})
 laser_screwdriver = misc.Weapon("Laser Screwdriver", constants.DMG_TYPE_MELEE, "light", 5, 12, {"crit": 3, "bonus": 1}, {})
 frostscythe_iii = misc.Weapon("Frostscythe III", constants.DMG_TYPE_SCYTHE, "ice", 92, 100, {"bpd": 5, "crit": 12, "mpm": 5, "WIS": 5, "END": 5, "CHA": 10, "LUK": 15, "INT": 20, "DEX": 20, "STR": 20, "bonus": 12}, {"all": 10, "health": -20})
+dragonblade = misc.Weapon("DragonBlade", constants.DMG_TYPE_MELEE, "metal", 5, 10, {"bonus": 1}, {})
